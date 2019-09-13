37866e502b3fe30e671fd6ab4c726fd65275d4fd
amir.ebrahimi <amir.ebrahimi@chalmers.se>
2019-09-13 16:59:58 +0200

    Adding entity classes

---
+++ ProductPlatformAnalyzer/Part.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductPlatformAnalyzer
+{
+    public class Part
+    {
+        public string Names { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Part lPart = (Part)obj;
+            return this.Names == lPart.Names;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Names.GetHashCode() * 5;
+        }
+    }
+}
---
+++ ProductPlatformAnalyzer/Trait.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductPlatformAnalyzer
+{
+    public class Trait
+    {
+        public string Names { get; set; }
+        public HashSet<Tuple<string,string>> Attributes { get; set; }
+        public HashSet<Trait> Inherit { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Trait lTrait = (Trait)obj;
+            return (this.Names == lTrait.Names);
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.Names.GetHashCode() * 7);
+        }
+
+    }
+}
